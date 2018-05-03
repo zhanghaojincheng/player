55d283fcfca0558a23f02d0848d7f3f011a477e7
zhanghaojincheng <714066825@qq.com>
2018-05-03 23:25:07 +0800

    first write code for mp3 player

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style scoped>
+
+#app {
+
+}
+</style>
