cdcb33aeb9db3d7514c94c48503d5f6184b64b34
zhanghaojincheng <714066825@qq.com>
2018-05-13 16:19:46 +0800

    歌手列表页面完成

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -5,7 +5,7 @@
           :probeType="probeType"
           @scroll="listenChangeScroll"
   >
-    <ul>
+    <ul class="ul-listview">
       <li class="list-group" v-for="group in data" :key="group.title" ref="listGroup">
         <h2 class="list-group-title">{{group.title}}</h2>
         <ul>
@@ -21,26 +21,23 @@
         <li class="item" :class="{current : index === touchIndex}" v-for="(item, index) in shortcutList" :data-index="index" :key="index">{{item}}</li>
       </ul>
     </div>
+    <div class="list-fixed" ref="fixed" v-show="fixedTitle">
+      <div class="fixed-title">{{fixedTitle}} </div>
+    </div>
   </scroll>
 </template>
 
 <script>
 import Scroll from 'base/scroll/scroll'
 import { attributeMethod } from 'common/js/dom'
+
+const TITLE_HEIGHT = 30
 export default {
   components: {
     Scroll
   },
   created () {
     this.touch = {}
-  },
-  mounted () {
-
-  },
-  updated () {
-    this._computedHeight()
-    this.letterHeight = this.$refs.letterList.firstChild.clientHeight
-    this.scrollView = this.$refs.listview
   },
   props: {
     data: {
@@ -53,7 +50,9 @@ export default {
       scrollY: -1,
       touchIndex: 0,
       listenScroll: true,
-      probeType: 3
+      probeType: 3,
+      letterHeightArray: [],
+      diff: -1
     }
   },
   computed: {
@@ -61,23 +60,32 @@ export default {
       return this.data.map((group) => {
         return group.title.substring(0, 1)
       })
+    },
+    fixedTitle: function () {
+      // console.log(this.scrollY)
+      if (this.scrollY > 0) {
+        return
+      }
+      return this.data[this.touchIndex] ? this.data[this.touchIndex].title : ''
     }
   },
   methods: {
     onShortcutTouchStart (e) {
       this.touch.y1 = e.touches[0].pageY
       this.tempIndex = attributeMethod(e.target, 'index')
-      this.touchIndex = +this.tempIndex
       this.scrollTo(this.tempIndex)
     },
     onShortcutTouchMove (e) {
       this.touch.y2 = e.touches[0].pageY
       let movedHeight = this.touch.y2 - this.touch.y1
       let moveNumber = Math.round(movedHeight / this.letterHeight) + (+this.tempIndex)
-      this.touchIndex = moveNumber
       this.scrollTo(moveNumber)
     },
     scrollTo (index) {
+      if (index === null || (index < 0) || (index > this.shortcutList.length - 1)) {
+        return
+      }
+      this.touchIndex = Number(index)
       this.scrollView.scrollToElement(this.$refs.listGroup[index], 0)
     },
     listenChangeScroll (pos) {
@@ -93,21 +101,39 @@ export default {
         arr.push(height)
       }
       this.letterHeightArray = arr
-      // console.log(arr)
+      console.log(arr)
     }
   },
   watch: {
-    scrollY: function () {
-      if (this.scrollY > 0) {
+    data () {
+      setTimeout(() => {
+        this._computedHeight()
+        this.letterHeight = this.$refs.letterList.firstChild.clientHeight
+        this.scrollView = this.$refs.listview
+      }, 0)
+    },
+    scrollY (newY) {
+      if (newY > 0) {
         this.touchIndex = 0
         return
       }
       for (let i = 0; i < this.letterHeightArray.length; i++) {
-        if (-this.scrollY < this.letterHeightArray[i] && -this.scrollY > this.letterHeightArray[i - 1]) {
+        let height1 = this.letterHeightArray[i]
+        let height2 = this.letterHeightArray[i - 1]
+        if (-newY <= height1 && -newY > height2) {
+          this.diff = height1 + newY
           this.touchIndex = i - 1
           break
         }
       }
+    },
+    diff(newY) {
+      let fixedTop = newY < TITLE_HEIGHT ? newY - TITLE_HEIGHT : 0
+      if (this.fixedTop === fixedTop) {
+        return
+      }
+      this.fixedTop = fixedTop
+      this.$refs.fixed.style.transform = `translate3d(0,${fixedTop}px,0)`
     }
   }
 }
@@ -122,27 +148,29 @@ export default {
     height: 100%
     overflow: hidden
     background: $color-background
-    .list-group
-      padding-bottom: 30px
-      .list-group-title
-        height: 30px
-        line-height: 30px
-        padding-left: 20px
-        font-size: $font-size-small
-        color: $color-text-l
-        background: $color-highlight-background
-      .list-group-item
-        display: flex
-        align-items: center
-        padding: 20px 0 0 30px
-        .avatar
-          width: 50px
-          height: 50px
-          border-radius: 50%
-        .name
-          margin-left: 20px
+    .ul-listview
+      /*padding-top:30px*/
+      .list-group
+        padding-bottom: 30px
+        .list-group-title
+          height: 30px
+          line-height: 30px
+          padding-left: 20px
+          font-size: $font-size-small
           color: $color-text-l
-          font-size: $font-size-medium
+          background: $color-highlight-background
+        .list-group-item
+          display: flex
+          align-items: center
+          padding: 20px 0 0 30px
+          .avatar
+            width: 50px
+            height: 50px
+            border-radius: 50%
+          .name
+            margin-left: 20px
+            color: $color-text-l
+            font-size: $font-size-medium
     .list-shortcut
       position: absolute
       z-index: 30
