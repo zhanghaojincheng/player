3a50bd5fa4bec469d2ac2a3d3a85e18409b6278f
zhanghaojincheng <714066825@qq.com>
2018-05-14 17:33:50 +0800

    vuex start

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store/index'
 import fastclick from 'fastclick'
 import VueLazyload from 'vue-lazyload'
 import 'common/stylus/index.styl'
